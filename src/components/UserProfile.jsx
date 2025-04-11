819f4a3ee4d594b1d8fd44f5a962c92b9140814a
pratik <pratikjena488@gmail.com>
2025-04-11 22:33:45 +0530

    fourth commit

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -29,7 +29,7 @@ const UserProfile = () => {
         <div>
           <p className="font-semibold text-xl dark:text-gray-200"> NANU </p>
           <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> Pratik </p>
+          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> WeCare </p>
         </div>
       </div>
       <div>
