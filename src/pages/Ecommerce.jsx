a872555a322602dcc2824ff797867dcaf875da2b
pratik105 <pratik@instasafe.com>
2025-04-14 19:31:41 +0530

    name commit

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -309,7 +309,7 @@ const Ecommerce = () => {
             />
             <div className="mt-8">
               <p className="font-semibold text-lg">Dboard is the future!</p>
-              <p className="text-gray-400 ">By Elon Musk</p>
+              <p className="text-gray-400 ">By Pratyusha </p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
                 here. There could be some great info.
